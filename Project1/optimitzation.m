596f6219683b1c165cc716a7d4489781e6fb4188
Walczakp007 <walczakp007@gmail.com>
2017-03-03 17:43:53 +0100

    No int constraint for lambda

--- Project1/optimitzation.m
+++ Project1/optimitzation.m
@@ -11,7 +11,7 @@ E(x(1),x(2),x(3),true);
 nVars = 3; % optimizing 3 params, N, Nu and Lambda
 lb = [1 1 0.1]; %lower bound
 ub = [150 150 150]; %upper bound
-[x,fval]= ga(@(x)(dmcE(x(1),x(2),x(3),false)),nVars,[],[],[],[],lb,ub,[],[1 2 3]);
+[x,fval]= ga(@(x)(dmcE(x(1),x(2),x(3),false)),nVars,[],[],[],[],lb,ub,[],[1 2]);
 %drawDMC
 dmcE(x(1),x(2),x(3),true);
 
@@ -19,3 +19,4 @@ dmcE(x(1),x(2),x(3),true);
 
 %optimalDMC error = 13.2748
 % N = 144, Nu = 2, Lambda = 4;
+
