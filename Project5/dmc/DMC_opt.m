7d4b521283a20b6c4198d0c5351072180539bf6a
Kamil Gabryjelski <kamil.gabryjelski@gmail.com>
2017-06-11 11:48:40 +0200

    kropki na przecinki

--- Project5/dmc/DMC_opt.m
+++ Project5/dmc/DMC_opt.m
@@ -1,7 +1,7 @@
-LB=[0,0,0];
-UB=[Inf,Inf,Inf];
+LB=[0 0 0 0 0 0 0];
+UB=[Inf,Inf,Inf,Inf,Inf,Inf,Inf];
 lambda=[1 1 1 1];
 psi=[1 1 1];
-x0=[psi];
-x=fmincon(@(x)(zad3DMC(80,40,5,[0.3 0.15 0.1 0.3],[x(1) x(2) x(3)],false, false)),x0,[],[],[],[],LB,UB);
-zad3DMC(80,40,5,[0.3 0.15 0.1 0.3],[x(1) x(2) x(3)],true, false)
+x0=[lambda psi];
+x=fmincon(@(x)(zad3DMC(80,40,5,[x(1) x(2) x(3) x(4)],[x(5) x(6) x(7)],false, false)),x0,[],[],[],[],LB,UB);
+zad3DMC(80,40,5,[x(1) x(2) x(3) x(4)],[x(5) x(6) x(7)],true, false)
